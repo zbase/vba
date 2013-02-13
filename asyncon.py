2abfcf545ba6e104e1a3e357e96a215510725075
Varun Dhussa <vdhussa@zynga.com>
2013-02-13 14:46:34 +0530

    Adding hbm monitoring support initial commit

---
+++ asyncon.py
@@ -0,0 +1,276 @@
+import errno
+import libevent
+import socket
+import os
+
+from errno import EALREADY, EINPROGRESS, EWOULDBLOCK, ECONNRESET, \
+     ENOTCONN, ESHUTDOWN, EINTR, EISCONN, errorcode
+
+"""
+    Wrapper class on libevent to provide an asyncore like interface
+"""
+class AsynCon(object):
+    EV_READ = libevent.EV_READ
+    EV_WRITE = libevent.EV_WRITE
+    EV_TIMEOUT = libevent.EV_TIMEOUT
+
+    def __init__(self):
+        self._base = libevent.Base()
+        self._map = {}
+
+    def loop(self):
+        self._base.loop()
+
+    def get_base(self):
+        return self._base
+
+    # For a map for conn objects that can be accessed by the app
+    # @optional
+    def add_connection(self, conObj):
+        self._map[conObj.fileno()] = conObj
+
+
+"""
+    Connection wrapper class 
+    handles connection creation, events etc
+"""
+class AsynConDispatcher(object):
+    def __init__(self, sock=None, timeout=5.0, mgr=None, outIp=None):
+        self.socket = None
+        self.read_event = None
+        self.write_event = None
+        self.timeout_event = None
+        self.addr = addr = None
+        self.connected = False
+        self._mgr = mgr
+        self._base = mgr._base
+        self.accepting = False
+        self.closing = False
+        self.timeout = 5#timeout
+        self._map = mgr._map
+        self.outIp = outIp
+
+        if not sock is None:
+            self.socket = sock
+            self.socket.setblocking(False)
+            self.connected = True
+            self.manage_socket()
+            self.create_events()
+            try:
+                self.addr = self.socket.getpeername()
+            except socket.error:
+                pass
+
+    def __repr__(self):
+        status = [self.__class__.__module__+"."+self.__class__.__name__]
+        if self.accepting and self.addr:
+            status.append('listening')
+        elif self.connected:
+            status.append('connected')
+        if self.addr is not None:
+            try:
+                status.append('%s:%d' % self.addr)
+            except TypeError:
+                status.append(repr(self.addr))
+        return '<%s at %#x>' % (' '.join(status), id(self))
+
+    def fileno(self):
+        return self._fileno
+
+    def manage_socket(self):
+        if not self.outIp is None:
+            self.socket.bind((self.outIp, 0))
+        self._fileno = self.socket.fileno()
+        self._map[self._fileno] = self.socket
+
+    def create_socket(self, family, type):
+        self.family_and_type = family, type
+        self.socket = socket.socket(family, type)
+        self.socket.setblocking(False)
+        self.manage_socket()
+
+    def set_reuse_addr(self):
+        # try to re-use a server port if possible
+        try:
+            self.socket.setsockopt(
+                socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                self.socket.getsockopt(socket.SOL_SOCKET,
+                                       socket.SO_REUSEADDR) | 1
+                )
+        except socket.error:
+            pass
+
+    # Filters for setting the socket as readable/writeable
+    # Read/write events will be added on timeout
+    # for compatibility (unused)
+    def readable(self):
+        return True
+
+    # for compatibility (unused)
+    def writable(self):
+        return True
+
+    def listen(self, num):
+        self.accepting = True
+        if os.name == 'nt' and num > 5:
+            num = 1
+        self.create_events()
+        return self.socket.listen(num)
+
+    def bind(self, addr):
+        self.addr = addr
+        return self.socket.bind(addr)
+
+    def connect(self, address):
+        self.connected = False
+        err = self.socket.connect_ex(address)
+        self.create_events()
+        # XXX Should interpret Winsock return values
+        if err in (EINPROGRESS, EALREADY, EWOULDBLOCK):
+            return
+        if err in (0, EISCONN):
+            self.addr = address
+            self.connected = True
+            self.handle_connect()
+        else:
+            raise socket.error, (err, errorcode[err])
+
+    def accept(self):
+        # XXX can return either an address pair or None
+        try:
+            conn, addr = self.socket.accept()
+            return conn, addr
+        except socket.error, why:
+            if why[0] == EWOULDBLOCK:
+                pass
+            else:
+                raise
+
+    def send(self, data):
+        try:
+            result = self.socket.send(data)
+            return result
+        except socket.error, why:
+            if why[0] == EWOULDBLOCK:
+                return 0
+            else:
+                raise
+            return 0
+
+    def recv(self, buffer_size):
+        try:
+            data = self.socket.recv(buffer_size)
+            if not data:
+                # a closed connection is indicated by signaling
+                # a read condition, and having recv() return 0.
+                self.handle_close()
+                return ''
+            else:
+                return data
+        except socket.error, why:
+            # winsock sometimes throws ENOTCONN
+            if why[0] in [ECONNRESET, ENOTCONN, ESHUTDOWN]:
+                self.handle_close()
+                return ''
+            else:
+                raise
+
+    def close(self):
+        self.read_event.delete()
+        self.write_event.delete()
+        self.timeout_event.delete()
+        self.socket.close()
+
+    def enable_read(self):
+        self.read_event.add()
+
+    def enable_write(self):
+        self.write_event.add()
+
+    def handle_read_event(self, evt, fd, what, obj):
+        if self.accepting:
+            # for an accepting socket, getting a read implies
+            # that we are connected
+            if not self.connected:
+                self.connected = True
+            self.handle_accept()
+        elif not self.connected:
+            self.handle_connect()
+            self.connected = True
+            self.handle_read()
+        else:
+            self.handle_read()
+
+    def handle_write_event(self, evt, fd, what, obj):
+        # getting a write implies that we are connected
+        if not self.connected:
+            self.handle_connect()
+            self.connected = True
+        self.handle_write()
+
+    def enable_timeout(self):
+        self.timeout_event.add(self.timeout)
+
+    def handle_timeout_event(self, evt, fd, what, obj):
+        if not self.connected:
+            self.log_info('connection not established','warning')
+        self.handle_timeout()
+
+    def handle_expt_event(self):
+        self.handle_expt()
+
+    def handle_error(self):
+        self.log_info('unimplemented handle error', 'error')
+        #handle error
+
+    def handle_expt(self):
+        self.log_info('unhandled exception', 'warning')
+
+    def handle_read(self):
+        self.log_info('unhandled read event', 'warning')
+
+    def handle_write(self):
+        self.log_info('unhandled write event', 'warning')
+
+    def handle_connect(self):
+        self.log_info('unhandled connect event', 'warning')
+
+    def handle_accept(self):
+        self.log_info('unhandled accept event', 'warning')
+
+    def handle_timeout(self):
+        self.log_info('unhandled timeout event', 'warning')
+
+    def handle_close(self):
+        self.log_info('unhandled close event', 'warning')
+        self.close()
+
+    def create_events(self):
+        self.write_event = libevent.Event(self._base, self._fileno, AsynCon.EV_WRITE, self.handle_write_event, self)
+        self.read_event = libevent.Event(self._base, self._fileno, AsynCon.EV_READ, self.handle_read_event, self)
+        self.timeout_event = libevent.Event(self._base, self._fileno, AsynCon.EV_TIMEOUT, self.handle_timeout_event, self)
+        #self.read_event.add()
+        #self.timeout_event.add(self.timeout)
+        #self.write_event.add()
+
+    def set_event(self, what, freq=None):
+        if what == AsynCon.EV_READ:
+            self.read_event.add(freq)
+        elif what == AsynCon.EV_WRITE:
+            self.write_event.add(freq)
+        elif what == AsynCon.EV_TIMEOUT:
+            self.timeout_event.add(freq)
+        else:
+            self.log_info("unhandled event add request", "warning")
+
+    # log and log_info may be overridden to provide more sophisticated
+    # logging and warning methods. In general, log is for 'hit' logging
+    # and 'log_info' is for informational, warning and error logging.
+
+    def log(self, message):
+        sys.stderr.write('log: %s\n' % str(message))
+
+    def log_info(self, message, type='info'):
+        if __debug__ or type != 'info':
+            print '%s: %s' % (type, message)
+
---
+++ membaseHandler.py
@@ -0,0 +1,234 @@
+import string
+import socket
+import exceptions
+import struct
+import errno
+import time
+
+from logger import *
+from asyncon import *
+
+Log = getLogger()
+
+class MembaseHandler(AsynConDispatcher):
+    VERSION_MONITORING = 1
+    STATS_MONITORING = 2
+    STATS_COMMAND = "START_STATS"
+    START_COMMAND = "START"
+    MEMBASE_PORT = 11211
+    ERROR_THRESHOLD = 5
+    HEALTHY = 1
+    FAIL = 2
+    RECONNECT = 3
+    STATS_TERM = "END"
+    ITEM_COUNT_KEY = "curr_items"
+    MEMBASE_VERSION_CMD = "version\r\n"
+    MEMBASE_STATS_CMD = "stats\r\n"
+    TOT_RETRY_COUNT = 3
+    CUR_ITEMS_DELTA = 500
+
+    def __init__(self, params):
+        sock = None
+        self.callback = None
+        self.ip = None
+        self.retry_count = MembaseHandler.TOT_RETRY_COUNT
+        self.failCallback = None
+        self.link = None
+        self.addr = None
+        self.port = None
+        self.local_item_count = -1
+        self.remote_item_count = -1
+        self.cmd_type = None
+        self.half_baked = True
+        self.send_count = 0
+        self.recv_count = 0
+        self.timeout = 10
+        port = None
+        self.map = None
+        self.mgr = None
+        self.aok_ts = time.time()
+        self.read_callback = None
+        self.monit_ip = None
+
+        if(params.has_key('ip')):
+            self.ip = params['ip']
+        if(params.has_key('monitIP')):
+            self.monit_ip = params['monitIP']
+        if(params.has_key('retryCount')):
+            self.retry_count = params['retryCount']
+        if(params.has_key('failCallback')):
+            self.failCallback = params['failCallback']
+        if(params.has_key('readCallback')):
+            self.read_callback = params['readCallback']
+        if(params.has_key("retry")):
+            self.retry_count = params["retry"]
+        if(params.has_key('addr')):
+            self.addr = params['addr']
+            self.ip = self.addr[0]
+            self.port = self.addr[1]
+        if(params.has_key('timeout')):
+            self.timeout = params['timeout']
+        if(params.has_key('port')):
+            port = params['port']
+            self.port = str(port)
+        else:
+            port = MembaseHandler.MEMBASE_PORT
+            self.port = str(MembaseHandler.MEMBASE_PORT)
+        if(params.has_key('map')):
+            self.map = params['map']
+        if(params.has_key('mgr')):
+            self.mgr = params['mgr']
+        if(params.has_key('type')):
+            if params['type'] == MembaseHandler.STATS_COMMAND :
+                self.command_type = MembaseHandler.STATS_MONITORING
+            else:
+                self.command_type = MembaseHandler.VERSION_MONITORING
+                self.half_baked = False
+
+        AsynConDispatcher.__init__(self, None, self.timeout, self.mgr)
+        #Creates non blocking socket
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self.addr is None:
+            self.connect((self.ip, port))
+        else:
+            self.connect(self.addr)
+
+        self.rbuf = ""
+        self.wbuf = ""
+        self.buffer_size = 4096 
+        self.closeSock = None 
+        self.gotSize = False
+        self.totSize = 0
+        self.ipStr = (None, str(self.ip))[self.ip != None]
+        self.ipStr += ("",":"+str(port))[port != None]
+        if not self.map is None:
+            self.map[self.ip+":"+self.port] = self
+
+    def handle_connect(self):
+        Log.debug("Connected %s:%s" %(self.ip, self.port))
+        pass
+
+    def isConnected(self):
+        return self.connected
+
+    def destroy(self):
+        if(self.connected == True):
+            self.connected = False
+            self.close()
+
+    def handle_error(self):
+        self.reconnect()
+
+    def handle_read(self):
+        while True:
+            try:
+                self.rbuf += self.recv(self.buffer_size)
+                Log.debug(self.rbuf)
+            except socket.error, why:
+                if why[0] == errno.EAGAIN:
+                    break
+                else:
+                    Log.error("Read event error %s" % why)
+                    self.rbuf = ""
+                    self.handle_error()
+                    return
+        if self.command_type == MembaseHandler.VERSION_MONITORING:   
+            self.handle_version_read()
+        else:
+            self.handle_stats_read()
+        if not self.read_callback is None:
+            self.read_callback(self)
+
+    def handle_stats_read(self):
+        if self.rbuf.find(MembaseHandler.STATS_TERM) > 0:
+            stats_map = {}
+            msg = self.rbuf[:len(self.rbuf)]
+            self.rbuf = ""
+            data_arr = msg.split("\r\n")
+            for row in data_arr:
+                data = row.split(' ')
+                if len(data) > 2:
+                    stats_map[data[1]] = data[2]
+            if stats_map.has_key(MembaseHandler.ITEM_COUNT_KEY):
+                self.remote_item_count = int(stats_map[MembaseHandler.ITEM_COUNT_KEY])
+                self.recv_count += 1
+                if self.local_item_count != -1 and (self.remote_item_count - self.local_item_count) < MembaseHandler.CUR_ITEMS_DELTA:
+                    self.half_baked = False
+
+    def handle_version_read(self):
+        if self.rbuf.find("VERSION") >= 0:
+            self.recv_count += 1
+            self.rbuf = ""
+
+    def write_data(self, msg):
+        self.wbuf += msg
+        self.enable_write()
+
+    def send_version(self):
+        self.write_data(MembaseHandler.MEMBASE_VERSION_CMD)
+
+    def send_stats(self):
+        self.write_data(MembaseHandler.MEMBASE_STATS_CMD)
+
+    def handle_write(self):
+        try:
+            if len(self.wbuf) > 0:
+                sent = self.send(self.wbuf)
+                self.wbuf = self.wbuf[sent:]
+                self.send_count += 1
+            else:
+                Log.debug("Write on empty buffer!")
+        except socket.error, why:
+            Log.error("Exception when trying to write %s. Reconnecting and retrying %s" %(why, self.wbuf))
+            self.reconnect()
+            self.enable_write()
+
+    def reconnect(self):
+        self.send_count = 0
+        self.recv_count = 0
+        self.retry_count -= 1
+        self.socket.close()
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((self.ip, int(self.port)))
+
+    def health(self):
+        if self.connected and (self.retry_count > 0) and ((self.send_count - self.recv_count) < MembaseHandler.ERROR_THRESHOLD):
+            Log.debug("Counts: s:%d r:%d" %(self.send_count, self.recv_count))
+            return MembaseHandler.HEALTHY
+        elif self.retry_count <= 0:
+            return MembaseHandler.FAIL
+        else:
+            return MembaseHandler.RECONNECT
+
+    def handle_timeout(self):
+        self.aok_ts = time.time()
+        con_health = self.health()
+        if con_health == MembaseHandler.HEALTHY:
+            # Check of connection is healthy
+            # Check sent and received count
+            if self.command_type == MembaseHandler.STATS_MONITORING:
+                Log.debug("Item counts: Local: %d Remote %d" %(self.local_item_count, self.remote_item_count))
+                if (self.remote_item_count >= 0) and (self.local_item_count > 0)  and ((self.remote_item_count - self.local_item_count) < MembaseHandler.CUR_ITEMS_DELTA):
+                    self.half_baked = False
+                    Log.info("Slave is fully functional")
+
+                if not self.half_baked:
+                    #Switch to command type VERSION
+                    Log.info("Switching to command type VERSION")
+                    self.command_type = MembaseHandler.VERSION_MONITORING
+                    self.send_version()
+                else:
+                    self.send_stats()
+            else:
+                self.send_version()
+        elif con_health == MembaseHandler.RECONNECT:
+            self.reconnect()
+        else:
+            self.failCallback(self)
+            self.close()
+                
+    def handle_close(self):
+        self.close()
+
+    def handler_error(self):
+        self.destroy()
